de8b38cc64a9cdddce89374eca4807789ff37114
Carles Capellas <capellas.carles@gmail.com>
2021-09-17 13:38:33 +0200

    chore: mock authentication

--- src-client/src/components/auth.tsx
+++ src-client/src/components/auth.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { getAuthToken, saveAuthToken, clearAuthToken } from '../storage';
+import { getAuthToken, saveAuthToken } from '../storage';
 
 interface AuthProps {
     setAuthToken: (authToken: string) => void;
 }
+
+const mockToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZ1dHVyZSIsImlhdCI6MTYzMTg3ODMxOCwiZXhwIjoxNjMxODg5MTE4fQ.UfTW_I3Zulav4RoWrJsZFHGJhQlDrLIejTuj25d3ibk';
 
 export const Auth: React.FC<AuthProps> = (props) => {
     const [errorMessage, setErrorMessage] = useState<string>();
@@ -14,31 +17,33 @@ export const Auth: React.FC<AuthProps> = (props) => {
     useEffect(() => {
         const savedAuthToken = getAuthToken();
         if (savedAuthToken) {
-            setIsLoading(true);
-            fetch('/api/refresh-token', {
-                headers: {
-                    Authorization: savedAuthToken
-                },
-                method: 'POST'
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        response.json().then((data) => {
-                            saveAuthToken(data.token);
-                            props.setAuthToken(data.token);
-                        });
-                    } else {
-                        // Saved token is invalid or expired
-                        clearAuthToken();
-                    }
-                })
-                .catch(() => {
-                    // If refresh fails, user will have to re-authenticate
-                    clearAuthToken();
-                })
-                .finally(() => {
-                    setIsLoading(false);
-                });
+            props.setAuthToken(savedAuthToken);
+
+            // setIsLoading(true);
+            // fetch('/api/refresh-token', {
+            //     headers: {
+            //         Authorization: savedAuthToken
+            //     },
+            //     method: 'POST'
+            // })
+            //     .then((response) => {
+            //         if (response.ok) {
+            //             response.json().then((data) => {
+            //                 saveAuthToken(data.token);
+            //                 props.setAuthToken(data.token);
+            //             });
+            //         } else {
+            //             // Saved token is invalid or expired
+            //             clearAuthToken();
+            //         }
+            //     })
+            //     .catch(() => {
+            //         // If refresh fails, user will have to re-authenticate
+            //         clearAuthToken();
+            //     })
+            //     .finally(() => {
+            //         setIsLoading(false);
+            //     });
         }
     }, []);
 
@@ -73,33 +78,47 @@ export const Auth: React.FC<AuthProps> = (props) => {
                     setErrorMessage(undefined);
                     setIsLoading(true);
 
-                    fetch('/api/auth', {
-                        body: JSON.stringify({
-                            username,
-                            password
-                        }),
-                        headers: {
-                            'content-type': 'application/json'
-                        },
-                        method: 'POST'
-                    })
-                        .then((response) => {
-                            if (response.ok) {
-                                response.json().then((data) => {
-                                    saveAuthToken(data.token);
-                                    props.setAuthToken(data.token);
-                                });
-                            } else {
-                                response.json().then((error) => setErrorMessage(error.message));
-                            }
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            setErrorMessage(error);
-                        })
-                        .finally(() => {
+                    if (!username) {
+                        setErrorMessage('Username is missing');
+                        setIsLoading(false);
+                    } else if (!password) {
+                        setErrorMessage('Password is missing');
+                        setIsLoading(false);
+                    } else {
+                        setTimeout(() => {
+                            saveAuthToken(mockToken);
+                            props.setAuthToken(mockToken);
                             setIsLoading(false);
-                        });
+                        }, 1000);
+                    }
+
+                    // fetch('/api/auth', {
+                    //     body: JSON.stringify({
+                    //         username,
+                    //         password
+                    //     }),
+                    //     headers: {
+                    //         'content-type': 'application/json'
+                    //     },
+                    //     method: 'POST'
+                    // })
+                    //     .then((response) => {
+                    //         if (response.ok) {
+                    //             response.json().then((data) => {
+                    //                 saveAuthToken(data.token);
+                    //                 props.setAuthToken(data.token);
+                    //             });
+                    //         } else {
+                    //             response.json().then((error) => setErrorMessage(error.message));
+                    //         }
+                    //     })
+                    //     .catch((error) => {
+                    //         console.log(error);
+                    //         setErrorMessage(error);
+                    //     })
+                    //     .finally(() => {
+                    //         setIsLoading(false);
+                    //     });
                 }}
                 type="button"
             >
